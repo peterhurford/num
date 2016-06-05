841b4e956122bad38703a9c5c1b548e903d8c55b
Peter Hurford <peter@peterhurford.com>
2016-06-04 23:59:14 -0500

    Refactor engine to not need rex and to work with decimals; add more tests; fix tests

--- R/num.R
+++ R/num.R
@@ -1,15 +1,9 @@
-is_comma_sep_num_rex <- rex::rex(start,
-  one_or_more(group(one_or_more(numbers), ",", one_or_more(numbers))), end)
-is_comma_sep_num <- function(str) {
-  grepl(is_comma_sep_num_rex, str)
-}
-
 abbreviations <- function() {
   list(
-    "K" = ",000",              # thousand
-    "M" = ",000,000",          # million
-    "B" = ",000,000,000",      # billion
-    "T" = ",000,000,000,000"   # trillion
+    "K" = 1000,           # thousand
+    "M" = 1000000,        # million
+    "B" = 1000000000,     # billion
+    "T" = 1000000000000   # trillion
   )
 }
 
@@ -25,12 +19,12 @@ num <- checkr::ensure(
     abbrev <- abbreviations()
     for (i in seq_along(abbrev)) {
       if (grepl(names(abbrev)[[i]], num)) {
-        num <- gsub(names(abbrev)[[i]], abbrev[[i]], num)
+        num <- gsub(names(abbrev)[[i]], paste("*", abbrev[[i]]), num)
       }
     }
-    if (is_comma_sep_num(num)) {
-      as.numeric(gsub(",", "", num))
-    } else {
+    tryCatch({
+      eval(parse(text = gsub(",", "", num)))
+    }, error = function(e) {
       stop("Could not parse.")
-    }
+    })
   })
--- tests/testthat/test-num.R
+++ tests/testthat/test-num.R
@@ -1,45 +1,57 @@
 context("num")
 library(testthat)
 library(magrittr)
-library(funtools)
+#library(funtools)
 library(checkr)
 
-comma_split_num <- ensure(
-  pre = x %is% numeric,
-  post = result %is% character,
-  function(x) {
-    withr::with_options(c("scipen" = 100, "digits" = 20), {
-      x %>% lsplit %>% rev %>% chunk(., 3) %/>% collapse %>%
-        join(",") %>% lsplit %>% rev %>% join
-    })
-  })
+#TODO: Finish when funtools is ready.
+# comma_split_num <- ensure(
+#   pre = x %is% numeric,
+#   post = result %is% character,
+#   function(x) {
+#     withr::with_options(c("scipen" = 100, "digits" = 20), {
+#       x %>% lsplit %>% rev %>% chunk(., 3) %/>% collapse %>%
+#         join(",") %>% lsplit %>% rev %>% join
+#     })
+#   })
 
-
+abbrev <- abbreviations()
 quickcheck_nums <- checkr::function_test_objects(pre = list(x %is% numeric, length(x) == 1))
 random_nums <- round(runif(100, 1, 100000000), 0)
 test_nums <- c(quickcheck_nums, random_nums)
 
-describe("comma separation is parsable", {
-  expect_true(all(sapply(test_nums, is.numeric)))
-  # Turns a bunch of numbers like 40000 into "40,000"
-  comma_test_nums <- lapply(test_nums, comma_split_num)
-  for (i in seq_along(test_nums)) {
-    test_that(paste(comma_test_nums[[i]], "is converted to", test_nums[[i]]),
-      expect_equal(num(comma_test_nums[[i]]), test_nums[[i]]))
+# describe("comma separation is parsable", {
+#   expect_true(all(sapply(test_nums, is.numeric)))
+#   # Turns a bunch of numbers like 40000 into "40,000"
+#   comma_test_nums <- lapply(test_nums, comma_split_num)
+#   for (i in seq_along(test_nums)) {
+#     test_that(paste(comma_test_nums[[i]], "is converted to", test_nums[[i]]),
+#       expect_equal(num(comma_test_nums[[i]]), test_nums[[i]]))
+#   }
+# })
+
+describe("letter interpolation is parsable", {
+  for (i in seq_along(abbrev)) {
+    describe(names(abbrev)[[i]], {
+      # Generates a bunch of stuff like 1000M, 100K, etc.
+      letter_nums <- unlist(test_nums) %>% paste0(names(abbrev)[[i]])
+      test_nums <- (unlist(test_nums) * abbrev[[i]]) %>% unname
+      for (j in seq_along(test_nums)) {
+        test_that(paste(letter_nums[[j]], "is converted to", test_nums[[j]]),
+          expect_equal(num(letter_nums[[j]]), test_nums[[j]]))
+      }
+    })
   }
 })
 
-describe("letter interpolation is possible", {
-   abbrev <- abbreviations()
-   for (i in seq_along(abbrev)) {
-     describe(names(abbrev)[[i]], {
-       # Generates a bunch of stuff like 1000M, 100K, etc.
-       letter_nums <- test_nums %>% paste0(names(abbrev)[[i]])
-       test_nums <- test_nums * abbrev[[i]]
-        for (j in seq_along(test_nums)) {
-          test_that(paste(letter_nums[[j]], "is converted to", test_nums[[j]]),
-            expect_equal(num(letter_nums[[j]]), test_nums[[j]]))
-        }
-     })
-   }
+describe("decimal separation is parsable with letters", {
+  random_nums <- round(runif(100, 1, 100000), 3)
+  for (i in seq_along(abbrev)) {
+    letter_nums <- random_nums %>% paste0(names(abbrev)[[i]])
+    test_nums <- unname(unlist(random_nums) * abbrev[[i]])
+    for (j in seq_along(test_nums)) {
+      test_that(paste(letter_nums[[j]], "is converted to", test_nums[[j]]),
+        expect_equal(num(letter_nums[[j]]), test_nums[[j]]))
+    }
+  }
 })
