1977c49fbbfc409e26bd01e92937c7f6cd241e66
Peter Hurford <peter@peterhurford.com>
2016-07-25 03:22:28 -0500

    Started activate_funtools_tests

--- tests/testthat/test-num.R
+++ tests/testthat/test-num.R
@@ -1,34 +1,30 @@
 context("num")
 library(testthat)
 library(magrittr)
-#library(funtools)
+library(funtools)
 library(checkr)
 
-#TODO: Finish when funtools is ready.
-# comma_split_num <- ensure(
-#   pre = x %is% numeric,
-#   post = result %is% character,
-#   function(x) {
-#     withr::with_options(c("scipen" = 100, "digits" = 20), {
-#       x %>% lsplit %>% rev %>% chunk(., 3) %/>% collapse %>%
-#         join(",") %>% lsplit %>% rev %>% join
-#     })
-#   })
+comma_split_num <- function(x) {
+    withr::with_options(c("scipen" = 100, "digits" = 20), {
+      x %>% as.character %>% lsplit %>% rev %>% chunk(., 3) %/>%
+        join %>% join(",") %>% lsplit %>% rev %>% join
+    })
+  }
 
 abbrev <- abbreviations()
 quickcheck_nums <- checkr::function_test_objects(pre = list(x %is% numeric, length(x) == 1))
 random_nums <- round(runif(100, 1, 100000000), 0)
-test_nums <- c(quickcheck_nums, random_nums)
+test_nums <- unname(unlist(c(quickcheck_nums, random_nums)))
 
-# describe("comma separation is parsable", {
-#   expect_true(all(sapply(test_nums, is.numeric)))
-#   # Turns a bunch of numbers like 40000 into "40,000"
-#   comma_test_nums <- lapply(test_nums, comma_split_num)
-#   for (i in seq_along(test_nums)) {
-#     test_that(paste(comma_test_nums[[i]], "is converted to", test_nums[[i]]),
-#       expect_equal(num(comma_test_nums[[i]]), test_nums[[i]]))
-#   }
-# })
+describe("comma separation is parsable", {
+  expect_true(all(sapply(test_nums, is.numeric)))
+  # Turns a bunch of numbers like 40000 into "40,000"
+  comma_test_nums <- lapply(test_nums, comma_split_num)
+  for (i in seq_along(test_nums)) {
+    test_that(paste(comma_test_nums[[i]], "is converted to", test_nums[[i]]),
+      expect_equal(num(comma_test_nums[[i]]), test_nums[[i]]))
+  }
+})
 
 describe("letter interpolation is parsable", {
   for (i in seq_along(abbrev)) {
